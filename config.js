a22a0e76bb7ce01583eb1d172ccff4c218080efc
ᴍ.ʀ sͥᴀͣᴅͫᴇᴇsʜᴀ <97608407+sadiyamin@users.noreply.github.com>
2024-01-28 23:59:18 -0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,134 @@
+const { readFileSync } = require('fs')
+require("dotenv").config();
+
+let badWords = [
+  "vagina",
+  "dick",
+  "mdrchod",
+  "mdrchod",
+  "chutiya",
+  "lodu",
+  "whore",
+  "hore",
+  "hoe",
+  "hoes",
+  "lode",
+  "cum",
+  "idiot",
+  "bastard",
+  "cunt",
+  "butt",
+  "pussy",
+  "chut",
+  "suck",
+  "scum",
+  "scumbag",
+  "niggr",
+  "nigga",
+  "chod",
+  "bhenchod",
+  "bc",
+  "bhodike",
+  "bsdk","randi",
+  "gandu",
+  "stfu",
+  "ass",
+  "asshole",
+  "madarchod",
+  "fuck",
+  "motherfucker",
+  "mother fucker",
+  "mf",
+  "mfs",
+  "fk",
+  "fck",
+  "gand",
+  "laund",
+  "loda",
+  "gulambi"];
+
+global.message = {
+    success: "✅ 𝚂𝚞𝚌𝚌𝚎𝚜𝚜! 𝙾𝚙𝚛𝚊𝚝𝚒𝚘𝚗 𝙲𝚘𝚖𝚙𝚕𝚎𝚝𝚎𝚍.",
+    admin: "*👤 A𝙳𝙼𝙸𝙽 N𝙴𝙴𝙳𝙴𝙳!*\n\n- Dear, this command is only for Admins. You have to be a admin in this group to use this command.",
+    botAdmin: "*🤖 B𝙾𝚃 A𝙳𝙼𝙸𝙽 N𝙴𝙴𝙳𝙴𝙳!*\n\n- I'm not an Admin, so I can't execute this command in this group. Please make me an Admin.",
+    owner: "*👑 O𝚆𝙽𝙴𝚁 N𝙴𝙴𝙴𝙳𝙴𝙳!*\n\n- Bruh, this command is only made for this bot's owner. So you can't use this command.",
+    group: "*👥 G𝚛𝚘𝚞𝚙 N𝚎𝚎𝚍𝚎𝚍!*\n\n- This command can only be executed in a group chat.",
+    private: 'This command is only for private chats.',
+    wait: '🔄 Processing request...',
+    link: 'I need a link to process this command.',
+    error: "❌ Oops! An error occurred while processing your request. Please try again later.",
+    ban: `You're banned from using this bot!`,
+    nsfw: 'This group is not *NSFW* enabled.',
+    banChat: 'This group is banned from using this bot, please contact owner to get unbanned.'
+},
+
+module.exports = {
+  botname: process.env.BotName || "Queen Bella", 
+  author: process.env.Author || "@PikaBotz",
+  packname: process.env.PackName || "Queen Bella v1",
+  socialLink: process.env.Web || "https://github.com/Sadiyamin",
+  footer: process.env.Footer || "© Queen Bella Bot",
+  prefa: process.env.Prefix || ['-'],
+  themeemoji: process.env.ThemeEmoji || "🎐",
+  ownername: process.env.Owner_Name || "Sadeesha",
+  ownernumber: process.env.Owner_Number || "94779062397",
+  instagramId: process.env.Insta || "8.08_only_mine",
+  warns: process.env.Warns_Limits || 3,
+  mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
+  welcome: process.env.Welcome_Msg || '*@$user* joined this group today as $membersth member.\n\n_$prefix welcome off to disable this message._',
+  left: process.env.Left_Msg || 'Ex-member *@$user* is no longer available in this group chat.\n\n_$prefix goodbye off to disable this message._',
+  promote: process.env.Promote_Msg || '*@$user* has been promoted as an admin in this group.\n\n_$prefix promotem off to disable this message._',
+  demote: process.env.Demote_Msg || '*@$user* has been demoted to a member in this group.\n\n_$prefix demotem off to disable this message._',
+  sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID", 
+  image_1: readFileSync('./lib/Assets/image_1.jpg'), // Thumbnail for allmenu command
+  image_2: readFileSync('./lib/Assets/image_2.jpg'), // null image
+  image_3: readFileSync("./lib/Assets/image_3.jpg"), // Thumbnail for Dashboard
+  aliveMedia: readFileSync("./lib/Assets/aliveMedia.mp4"),
+  menuMedia: readFileSync('./lib/Assets/menuMedia.mp4'),
+  badWords: badWords,
+  message: {
+    success: message.success,
+    admin: message.admin,
+    botAdmin: message.botAdmin,
+    owner: message.owner,
+    group: message.group,
+    private: message.private,
+    wait: message.wait,
+    link: message.link,
+    error: message.error,
+    ban: message.ban,
+    nsfw: message.nsfw,
+    banChat: message.banChat
+  },
+}
+
+
+
+// Ignore them 👇🏻
+global.botname = process.env.BotName || "Queen Bella" 
+global.author = process.env.Author || "@Sadeesha" 
+global.packname = process.env.PackName || "Queen Bella v1" 
+global.myweb = process.env.Web || "https://github.com/Sadiyamin" 
+global.footer = process.env.Footer || "© Queen Bella Bot" 
+global.prefa = process.env.Prefix || ['.'] 
+global.themeemoji = process.env.ThemeEmoji || "🎐" 
+global.ownername = process.env.Owner_Name || "Sadeesha" 
+global.ownernumber = process.env.Owner_Number || "94779062397" 
+global.adress = process.env.Continent || "Asia, Srilanka, Mathara" 
+global.timezone = process.env.TimeZone || "Asia/Colombo" 
+global.instagramId = process.env.Insta || "8.08_only_mine" 
+global.email = process.env.Email_Id || "sadiyamin7@gmail.com" 
+  
+//--------------- Tip ----------------\\
+global.Tips = [
+`Type *$prefix info* for more information....`,
+`Type *$prefix settings* to commit changes in the bot.`,
+`If you got a bug or error, then please report to developer asap by *$prefix report* command.`
+]
+
+//--------------- Menu images ----------------\\
+global.image_1 = readFileSync('./lib/Assets/image_1.jpg') // Thumbnail for allmenu command
+global.image_2 = readFileSync('./lib/Assets/image_2.jpg') // null image
+global.image_3 = readFileSync("./lib/Assets/image_3.jpg") // Thumbnail for Dashboard
+global.menu_pic = "https://i.ibb.co/PhDcZTM/Thumbnail.png";
+
